81a6618c6ef28f67dbb787b4ba8222e69d64a221
Rajani Lamichhane <83020452+Rajani1-tech@users.noreply.github.com>
2023-03-22 15:59:06 +0545

    Add files via upload

---
+++ Static.dart
@@ -0,0 +1,21 @@
+class Employee{
+    static int count = 0;
+    Employee(){
+        count++;
+    }
+
+void totalEmployee(){
+    print("Total Employee: $count");
+}
+}
+void main(){
+    Employee e1 = new Employee();
+    e1.totalEmployee();
+
+     Employee e2 = new Employee();
+      e2.totalEmployee();
+
+      Employee e3 = new Employee();
+       e3.totalEmployee();
+
+}
